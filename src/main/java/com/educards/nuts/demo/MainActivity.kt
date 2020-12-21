793b6ce55359cfaf6b3837ea239ea1a0f71ba76c
Michal Vician <vicianm@gmail.com>
2020-12-21 17:47:55 +0100

    Debug call button

--- src/main/java/com/educards/nuts/demo/MainActivity.kt
+++ src/main/java/com/educards/nuts/demo/MainActivity.kt
@@ -2,12 +2,18 @@ package com.educards.nuts.demo
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil.inflate
+import com.educards.nuts.demo.databinding.ActivityMainBinding
 
 class MainActivity: Activity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        inflate(layoutInflater, R.layout.activity_main, null, false) as ActivityMainBinding
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
 }
